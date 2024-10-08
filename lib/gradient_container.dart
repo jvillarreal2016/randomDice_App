a334d3f537cec926fff0e5741174bcbde826c6ca
Javi Villarreal <javivillarreal@Javis-MacBook-Pro-2.local>
2024-10-07 23:33:42 -0500

    random dice roll functionality

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:dice_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -23,10 +24,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Image.asset('assets/images/dice-2.png'),
-      ),
+      child: const Center(child: DiceRoller()),
     );
+    ;
   }
 }
 
